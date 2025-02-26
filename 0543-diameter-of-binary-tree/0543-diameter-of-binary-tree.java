1dd6f1b68a685cd6f319a5e10dcbef8364748993
subhang-IITD <137153239+subhang-IITD@users.noreply.github.com>
2025-02-26 17:37:43 +0530

    Time: 0 ms (100%), Space: 45 MB (38.16%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -17,7 +17,7 @@ class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         int[] result = new int[]{Integer.MIN_VALUE}; // Mutable reference
         solve(root, result);
-        return result[0]; // Returning diameter in terms of edges
+        return result[0]-1; // Returning diameter in terms of edges
     }
 
     private int solve(TreeNode root, int[] result) {
@@ -27,7 +27,7 @@ class Solution {
         int rightdia = solve(root.right, result);
 
         int temp = 1 + Math.max(leftdia, rightdia);
-        int ans =  leftdia + rightdia; // Diameter is counted in edges (not nodes)
+        int ans = Math.max(temp,1+leftdia+rightdia); // Diameter is counted in edges (not nodes)
         result[0] = Math.max(result[0], ans); // Update global result
 
         return temp;
