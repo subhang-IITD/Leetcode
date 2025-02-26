0c2c19185b687b6752839b611b840deeaa00379e
subhang-IITD <137153239+subhang-IITD@users.noreply.github.com>
2025-02-26 17:19:32 +0530

    Time: 0 ms (100%), Space: 44.5 MB (43.55%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return solve(root) != -1;
+    }
+    private int solve(TreeNode root){
+        if(root== null) return 0;
+        int lh = solve(root.left);
+        int rh = solve(root.right);
+        if(lh==-1 || rh == -1){
+            return -1;
+        }
+        if(Math.abs(lh-rh)>1) return -1;
+        return 1 + Math.max(lh,rh);
+    }
+}
