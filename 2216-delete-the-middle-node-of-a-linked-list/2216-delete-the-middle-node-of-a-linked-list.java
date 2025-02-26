40a2cf20d13c169027ac9df40f778d26471c1725
subhang-IITD <137153239+subhang-IITD@users.noreply.github.com>
2025-02-27 04:22:24 +0530

    Time: 3 ms (99.76%), Space: 65.7 MB (9.59%) - LeetHub

---
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        
+        while(fast !=null && fast.next!= null){
+            slow= slow.next;
+            fast = fast.next.next;
+        }
+        ListNode temp = head;
+        while(temp.next != slow){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+}
